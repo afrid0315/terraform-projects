84cdb263c7c19ed399092e68d1f99911a46ccfef
Afrid Basha Abdul <afridbashaabdul@gmail.com>
2023-10-28 18:47:12 +0530

    Update ec2_instance_creation.tf

--- ec2_instance_creation.tf
+++ ec2_instance_creation.tf
@@ -1,10 +1,10 @@
 provider "aws"{
-    resource "aws_instances" "this" {
+    region                      = "us-east-1"
+}
+resource "aws_instances" "this" {
         ami                     = "ami-0dbc3d7bc646e8516"
         instance_type           = "t2.micro"
         subnet_id               = "subnet-05867f98847f32488"
-    }
-    tags{
-        name = "terraform_ec2_creation"
-    }
+        key_name                = "mykey"
 }
+    
