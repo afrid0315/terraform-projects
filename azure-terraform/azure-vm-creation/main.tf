84a8d89ef71df6b7ec0aff4a4868f29118b6b75e
Afrid Basha Abdul <afridbashaabdul@gmail.com>
2023-12-31 19:33:46 +0530

    Update main.tf

--- azure-terraform/azure-vm-creation/main.tf
+++ azure-terraform/azure-vm-creation/main.tf
@@ -1,85 +1,12 @@
 provider "azurerm" {
   features {}
-} 
-resource "azurerm_resource_group" "example" {
-  name     = "myVmResource"
-  location = "East US"
 }
 
-resource "azurerm_virtual_network" "example" {
-  name                = "my-vm-network"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-}
-
-resource "azurerm_subnet" "example" {
-  name                 = "my-subnet"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
-
-resource "azurerm_ssh_public_key" "example" {
-  name                = "example"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
-  public_key          = file("~/.ssh/id_rsa.pub")
-}
-
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-
-  ip_configuration {
-    name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
-    private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.example.id
-  }
-}
-
-resource "azurerm_public_ip" "example" {
-  name                = "example-public-ip"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
-  allocation_method   = "Dynamic"
-}
-
-resource "azurerm_virtual_machine" "example" {
-  name                  = "example-vm"
-  location              = azurerm_resource_group.example.location
-  resource_group_name   = azurerm_resource_group.example.name
-  network_interface_ids = [azurerm_network_interface.example.id]
-
-  vm_size              = "Standard_B1s"
-  delete_os_disk_on_termination = true
-
-  storage_image_reference {
-    publisher = "Canonical"
-    offer     = "UbuntuServer"
-    sku       = "18.04-LTS"
-    version   = "latest"
-  }
-
-  storage_os_disk {
-    name              = "my-osdisk"
-    caching           = "ReadWrite"
-    create_option     = "FromImage"
-    managed_disk_type = "Standard_LRS"
-  }
-
-  os_profile {
-    computer_name  = "example-vm"
-    admin_username = "afriduser"
-  }
-
-  os_profile_linux_config {
-    disable_password_authentication = true
-    ssh_keys {
-    path     = "/home/afriduser/.ssh/authorized_keys"
-    key_data = file("/home/codespace/.ssh/id_rsa.pub")
-    }
-  }
+module "vm-module" {
+  source  = "./vm-module/"
+  
+  resource_group_name         = "myVmResource"
+  location                    = "East US"
+  ssh_public_key_path         = "~/.ssh/id_rsa.pub"
+  ssh_authorized_keys_path    = "/home/codespace/.ssh/id_rsa.pub"
 }
